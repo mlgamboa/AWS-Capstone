1b8fc175756cc2123f799c3daa915b3234886abd
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-10-07 08:58:40 +0800

    added some functions for testability

--- handler.js
+++ handler.js
@@ -7,4 +7,25 @@ const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
 
 app.use(express.json());
 
+app.get("/", (req, res, next) => {
+	return res.status(200).json({
+		message: "Hello from root!",
+	});
+});
+
+app.get("/hello", (req, res, next) => {
+	return res.status(200).json({
+		message: "Hello from path!",
+	});
+});
+
+app.post("/post", (req, res, next) => {
+	const bodyObj = JSON.parse(req.body.toString());
+	console.log(bodyObj);
+	console.log(req.headers.authorization);
+	return res.status(200).json({
+		message: "Hello from post!",
+	});
+});
+
 module.exports.handler = serverless(app);
