17c1ed0bc91e66b1df7d0b912fde34baa9ad21ae
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-10-07 10:45:09 +0800

    updates to jwtHelper

--- Helpers/jwtHelper.js
+++ Helpers/jwtHelper.js
@@ -1,8 +1,17 @@
-const jwtHelper = { getEmployeeEmailFromToken, generateToken };
+const jwt = require("jsonwebtoken");
+
+const { JWT_OPTIONS } = require("../Env/constants");
+const responsesHelper = require("../Helpers/responsesHelper");
+
+const jwtHelper = { getEmployeeEmailFromToken, generateToken, verifyToken };
 module.exports = jwtHelper;
 
 function getEmployeeEmailFromToken(token) {
 	return jwt.decode(token)["sub"];
+}
+
+function getAudienceFromToken(token) {
+	return jwt.decode(token)["aud"];
 }
 
 //TODO: fix db call
@@ -34,4 +43,27 @@ async function generateToken(prevToken, userEmail) {
 		audience: audience,
 	};
 	return jwt.sign({}, process.env.SECRET, options);
+}
+
+function verifyToken(req, res, next) {
+	const token = req.cookies.token;
+	if (!token) {
+		res.status(401).json({
+			...responsesHelper.unathorizedResponseBuilder(
+				"Not authorized to access data"
+			),
+		});
+	} else {
+		jwt.verify(token, process.env.SECRET, function (err) {
+			if (err) {
+				console.error(err);
+				res.clearCookie("token");
+				res.status(401).json({
+					...responsesHelper.unathorizedResponseBuilder(
+						"Please login again"
+					),
+				});
+			} else next();
+		});
+	}
 }
