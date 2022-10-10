c2d6238e41ee7ff316c2ea4cd3f724059e67ada6
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-10-10 21:24:44 +0800

    fixed getting the latest flex cutoff

--- DataAccess/Database/dbFlexCycleCutoff.js
+++ DataAccess/Database/dbFlexCycleCutoff.js
@@ -1,8 +1,33 @@
+const AWS = require("aws-sdk");
+const flexCycleCutoffModel = require("../../Models/flexCycleCutoffModel");
+
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+
 const dbFlexCycleCutoff = { getLatestFlexCycle, getFlexCycleById };
 module.exports = dbFlexCycleCutoff;
 
 async function getLatestFlexCycle() {
-	//TODO FIX ME
+	try {
+		const params = {
+			TableName: process.env.REIMBURSEMENT_TABLE,
+			KeyConditionExpression: "PK = :pk and begins_with(SK, :sk)",
+			ProjectionExpression: "CTF_id",
+			ExpressionAttributeValues: {
+				":pk": "true",
+				":sk": "CTF#",
+			},
+			Limit: 1,
+		};
+		const singleResultArr = await dynamoDbClient.query(params).promise();
+		let flexCycle = null;
+		if (singleResultArr.Items.length === 1) {
+			flexCycle = new flexCycleCutoffModel();
+			flexCycle.flexCutoffId = singleResultArr.Items[0].CTF_id;
+		}
+		return flexCycle;
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 async function getFlexCycleById(id) {
