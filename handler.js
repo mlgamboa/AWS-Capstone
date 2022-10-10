c83cfe895f0e2b196569e9fb26ded687cbb1d04e
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-10-10 17:19:37 +0800

    added endpoint

--- handler.js
+++ handler.js
@@ -5,11 +5,14 @@ const app = express();
 
 const errorHelper = require("./Helpers/errorHelper");
 const userRoutes = require("./Routes/userRoutes");
+const reimbursementRoutes = require("./Routes/reimbursementRoutes");
 
 app.use(express.json());
 
 app.post("/login", userRoutes.login);
 app.get("/logout", userRoutes.logout);
+
+app.get("/file-detail", reimbursementRoutes.file);
 
 app.use(errorHelper.logErrorsToConsole);
 app.use(errorHelper.clientErrorHandler);
