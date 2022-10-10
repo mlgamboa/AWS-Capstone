3140ee382e4ee95cfd71e449731acc19d75b6eb0
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-10-10 23:16:12 +0800

    added get cutoff by id

--- DataAccess/Database/dbFlexCycleCutoff.js
+++ DataAccess/Database/dbFlexCycleCutoff.js
@@ -2,6 +2,7 @@ const AWS = require("aws-sdk");
 const flexCycleCutoffModel = require("../../Models/flexCycleCutoffModel");
 
 const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+const REIMBURSEMENT_TABLE = process.env.REIMBURSEMENT_TABLE;
 
 const dbFlexCycleCutoff = { getLatestFlexCycle, getFlexCycleById };
 module.exports = dbFlexCycleCutoff;
@@ -9,7 +10,7 @@ module.exports = dbFlexCycleCutoff;
 async function getLatestFlexCycle() {
 	try {
 		const params = {
-			TableName: process.env.REIMBURSEMENT_TABLE,
+			TableName: REIMBURSEMENT_TABLE,
 			KeyConditionExpression: "PK = :pk and begins_with(SK, :sk)",
 			ProjectionExpression: "CTF_id",
 			ExpressionAttributeValues: {
@@ -31,5 +32,28 @@ async function getLatestFlexCycle() {
 }
 
 async function getFlexCycleById(id) {
-	//TODO FIX ME
+	try {
+		const params = {
+			TableName: REIMBURSEMENT_TABLE,
+			IndexName: "SK-CTF_id-index",
+			KeyConditionExpression: "SK = :sk and CTF_id = :ctf",
+			ProjectionExpression: "amount",
+			ExpressionAttributeValues: {
+				":sk": `CTF#${id}`,
+				":ctf": id,
+			},
+		};
+		const singleResultArr = await dynamoDbClient.query(params).promise();
+		let flexCycle = null;
+		if (singleResultArr.Items.length === 1) {
+			flexCycle = new flexCycleCutoffModel();
+			flexCycle.cutoffCapAmount = parseInt(
+				singleResultArr.Items[0].amount
+			);
+		}
+
+		return flexCycle;
+	} catch (error) {
+		console.log(error);
+	}
 }
