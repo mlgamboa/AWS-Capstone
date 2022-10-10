8de8e767d8d82d9417edf222ec44cbf7588b8824
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-10-10 15:54:40 +0800

    commented out unused code

--- Helpers/reimbursementHelper.js
+++ Helpers/reimbursementHelper.js
@@ -1,6 +1,5 @@
 const dbReimbursement = require("../DataAccess/Database/dbReimbursement");
 const dbReimbDetails = require("../DataAccess/Database/dbReimbDetails");
-const jwtHelper = require("../Helpers/jwtHelper");
 
 const reimbursementHelper = {
 	makeDraftReimbursement,
@@ -9,29 +8,29 @@ const reimbursementHelper = {
 module.exports = reimbursementHelper;
 
 async function makeDraftReimbursement(empId) {
-	//TODO get employee id from db using
 	//TODO const cutoff = get get latest active cut off
-	await dbReimbursement.add(empId, cutoff.cutoffId);
+	const latestFlexCycleCutoff = await dbFlexCycleCutoff.getLatestFlexCycle();
+	await dbReimbursement.add(empId, latestFlexCycleCutoff.cutoffId);
 }
 
-async function calculateReimbursementAmount(reimbursementId) {
-	const reimbDetailsArr = await dbReimbDetails.getDetailsByReimbId(
-		reimbursementId
-	);
+// async function calculateReimbursementAmount(reimbursementId) {
+// 	const reimbDetailsArr = await dbReimbDetails.getDetailsByReimbId(
+// 		reimbursementId
+// 	);
 
-	let totalAmount = 0;
+// 	let totalAmount = 0;
 
-	reimbDetailsArr.forEach(element => {
-		totalAmount += element.amount;
-	});
+// 	reimbDetailsArr.forEach(element => {
+// 		totalAmount += element.amount;
+// 	});
 
-	await dbReimbursement.updateReimbursementAmount(
-		reimbursementId,
-		totalAmount
-	);
+// 	await dbReimbursement.updateReimbursementAmount(
+// 		reimbursementId,
+// 		totalAmount
+// 	);
 
-	return totalAmount;
-}
+// 	return totalAmount;
+// }
 
 async function generateTransactionNumber(reimbursement) {
 	// TODO const company = get company
