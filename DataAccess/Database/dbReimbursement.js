3072cd146fddacb125279fad7d407d8255d63e20
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-10-10 19:51:51 +0800

    added function

--- DataAccess/Database/dbReimbursement.js
+++ DataAccess/Database/dbReimbursement.js
@@ -1,5 +1,7 @@
 const AWS = require("aws-sdk");
+const reimbursementModel = require("../../Models/reimbursementModel");
 
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
 
 const dbReimbursement = {
 	getLatestDraftByEmpId,
@@ -8,7 +10,34 @@ const dbReimbursement = {
 };
 module.exports = dbReimbursement;
 
-async function getLatestDraftByEmpId() {}
+async function getLatestDraftByEmpId(empId) {
+	try {
+		const params = {
+			TableName: process.env.REIMBURSEMENT_TABLE,
+			IndexName: "PK-GSI4_SK-index",
+			KeyConditionExpression: "PK = :pk and GSI4_SK = :gsi4sk",
+			ProjectionExpression: "amount, CTF_id, RMBRSMNT_id", // -- to filter out the output
+			ExpressionAttributeValues: {
+				":pk": `EMP#${empId}`,
+				":gsi4sk": `EMP#${empId}#draft`, //EMP#36#approved
+			},
+		};
+		const singleResultArr = await dynamoDbClient.query(params).promise();
+
+		let reimbursement = null;
+		if (singleResultArr.Items.length === 1) {
+			reimbursement = new reimbursementModel();
+			reimbursement.totalReimbursementAmount =
+				singleResultArr.Items[0].amount;
+			reimbursement.flexCutoffId = singleResultArr.Items[0].CTF_id;
+			reimbursement.flexReimbursementId =
+				singleResultArr.Items[0].RMBRSMNT_id;
+		}
+		return reimbursement;
+	} catch (error) {
+		console.log(error);
+	}
+}
 
 async function add(empId, cutoffId) {}
 
