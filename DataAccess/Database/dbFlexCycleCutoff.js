6db2dcd1f95495552348e6362dd661eaf8ed8f55
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-10-10 15:53:54 +0800

    added file

---
+++ DataAccess/Database/dbFlexCycleCutoff.js
@@ -0,0 +1,10 @@
+const dbFlexCycleCutoff = { getLatestFlexCycle, getFlexCycleById };
+module.exports = dbFlexCycleCutoff;
+
+async function getLatestFlexCycle() {
+	//TODO FIX ME
+}
+
+async function getFlexCycleById(id) {
+	//TODO FIX ME
+}
