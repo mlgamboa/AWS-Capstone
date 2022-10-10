ca3bb64e88a638bfa9f5c7edc435189d216290ca
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-10-11 00:25:22 +0800

    fixed

--- DataAccess/Database/dbReimbDetails.js
+++ DataAccess/Database/dbReimbDetails.js
@@ -2,9 +2,16 @@ const AWS = require("aws-sdk");
 const dbReimbDetails = { getDetailsByReimbId, file };
 module.exports = dbReimbDetails;
 
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+const REIMBURSEMENT_TABLE = process.env.REIMBURSEMENT_TABLE;
+
 async function file(reimbDetail) {
 	try {
-		const params = {};
+		const params = {
+			TableName: REIMBURSEMENT_TABLE,
+			Item: reimbDetail,
+		};
+		const singleResultArr = await dynamoDbClient.put(params).promise();
 	} catch (error) {}
 }
 async function getDetailsByReimbId(reimbId) {}
