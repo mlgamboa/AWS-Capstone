a18cd1f83b3076d6cd3ca071be99c14799a1a931
Arick Yumol <arick.yumol@pointwest.com.ph>
2022-10-11 15:49:01 +0800

    Add hr get by cutoffId, reimbursementId, employee, and flexpoint

--- DataAccess/Database/dbReimbursement.js
+++ DataAccess/Database/dbReimbursement.js
@@ -4,6 +4,9 @@ const dbReimbursement = {
 	getLatestDraftByEmpId,
 	add,
 	updateReimbursementAmount,
+	getReimbursementByCutoffId,
+	getReimbursmentAndDetailsByReimbursementId,
+	getReimbursmentAndDetailsByEmployee
 };
 module.exports = dbReimbursement;
 
@@ -79,4 +82,52 @@ async function updateReimbursementSubmitted(
 			":amt": totalAmount,
 		},
 	};
+}
+
+async function getReimbursementByCutoffId(cutoffId) {	// US009
+	const params = {
+		TableName: REIMBURSEMENT_TABLE,
+      	IndexName: 'CTF_id-SK-index',
+      	KeyConditionExpression: 'CTF_id = :ctf AND begins_with(SK, :sk)',
+      	ExpressionAttributeValues: {
+        	':ctf':`${cutoffId}`,
+        	':sk':'RMBRSMNT#'
+      	}
+	};
+	const data = await dynamoDbClient.query(params).promise();
+	return data;
+}
+
+async function getReimbursmentAndDetailsByReimbursementId(reimbursementId) {	// US0010
+	const params = {
+		TableName: REIMBURSEMENT_TABLE,
+		IndexName: 'RMBRSMNT_id-SK-index',
+		KeyConditionExpression: 'RMBRSMNT_id = :id AND begins_with(SK, :sk)',
+		ExpressionAttributeValues: {
+		  	':id':`${reimbursementId}`,
+		  	':sk':`RMBRSMNT#${reimbursementId}`
+		},
+		ProjectionExpression: 'SK, RMB_status, amount, transaction_number, CTF_id, date_submitted, tin_of_establishment, name_of_establishment, or_number, category'
+	};
+	const data = await dynamoDbClient.query(params).promise();
+	return data;
+}
+
+async function getReimbursmentAndDetailsByEmployee(cutoffId, employeeId, lastName, firstName) {	// US0011
+	const params = {
+		TableName: REIMBURSEMENT_TABLE,
+		IndexName: 'GSI6_PK-SK-index',
+		KeyConditionExpression: 'GSI6_PK = :ctf AND begins_with(SK, :sk)',
+		FilterExpression: 'PK = :pk OR begins_with(last_name, :ln) OR begins_with(first_name, :fn)',
+		ExpressionAttributeValues: {
+		  	':ctf':`${cutoffId}`,
+		  	':sk':'RMBRSMNT#',
+		  	':pk':`EMP#${employeeId}`,
+		  	':ln':`${lastName}`,
+		  	':fn':`${firstName}`
+		},
+		ProjectionExpression: 'SK, RMB_status, amount, transaction_number, CTF_id, date_submitted, tin_of_establishment, name_of_establishment, or_number, category, PK, first_name, last_name'
+	  };
+	const data = await dynamoDbClient.query(params).promise();
+	return data;
 }
